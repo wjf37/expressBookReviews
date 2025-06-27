27b7037ecbb9c2696e8e047c38b3ea578822d465
wjf37 <vincentw3007@gmail.com>
2025-06-27 11:53:53 -0400

    tasks 1 to 3 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,22 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  isbn = req.params.isbn;
+
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  author = req.params.author;
+  let filtered_books = Object.entries(books).filter(([,book]) => book.author === author);
+  res.send(filtered_books);
 });
 
 // Get all books based on title
